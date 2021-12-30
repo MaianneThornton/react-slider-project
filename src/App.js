3373eb72a1b18e54227f5bd9a208a82932d2422e
Maianne Thornton <maianne.thornton@gmail.com>
2021-12-30 15:38:11 -0500

    fixed the bug that allowed for scrolling past the number of indicies (positive and negative)

--- src/App.js
+++ src/App.js
@@ -5,6 +5,22 @@ import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  // fixing the bug that allowed us to scroll past the number of indexes in the data.js file (both positive and negative).
+  useEffect(() => {
+    // selecting the last index in the array
+    const lastIndex = people.length - 1;
+    // if the index is less than 0 set it to the lastIndex
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex){
+      setIndex(0);
+    }
+  }, [index, people]);
+
+
+
   return (
     <section className="section">
       {/* later move the title section into a separate component */}
@@ -20,16 +36,16 @@ function App() {
           const { id, image, name, title, quote } = person;
           // conditions for changing the slide positions
           let position = 'nextSlide';
-          if(personIndex === index){
+          if (personIndex === index) {
             position = 'activeSlide';
           }
-          // if the personIndex equals index -1 or 0 AND personIndex equals the total number of items in the array -1 add the class of lastSlide
-          if(personIndex === index - 1 || (index === 0 && personIndex === people.length -1)){
+          // if the personIndex equals index -1 OR 0 AND personIndex equals the total number of items in the array -1 add the class of lastSlide
+          if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
             position = 'lastSlide'
           }
           // later move the person section into a separate component
           return (
-            <article className= {position} key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={name} className='person-img' />
               <h4>{name}</h4>
               <p className="title">{title}</p>
@@ -39,10 +55,12 @@ function App() {
           );
         })}
         {/* later move the buttons into a separate component */}
-        <button className="prev" onClick={() => setIndex(index - 1)}>
+        <button className="prev"
+          onClick={() => setIndex(index - 1)}>
           <FiChevronLeft />
         </button>
-        <button className="next" onClick={() => setIndex (index + 1)}>
+        <button className="next"
+          onClick={() => setIndex(index + 1)}>
           <FiChevronRight />
         </button>
       </div>
