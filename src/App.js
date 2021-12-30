451a24b6dc36119b89e7b602cab91a91bc839aba
Maianne Thornton <maianne.thornton@gmail.com>
2021-12-30 15:23:21 -0500

    added classes to slides

--- src/App.js
+++ src/App.js
@@ -15,22 +15,30 @@ function App() {
       </div>
       <div className="section-center">
         {/* iterating the data.js file */}
-        {people.map((person, personIndex)=> {
+        {people.map((person, personIndex) => {
           // destructring the properties from the data.js file
-          const {id, image, name, title, quote} = person;
-          // more stuff coming up
+          const { id, image, name, title, quote } = person;
+          // conditions for changing the slide positions
+          let position = 'nextSlide';
+          if(personIndex === index){
+            position = 'activeSlide';
+          }
+          // if the personIndex equals index -1 or 0 AND personIndex equals the total number of items in the array -1 add the class of lastSlide
+          if(personIndex === index - 1 || (index === 0 && personIndex === people.length -1)){
+            position = 'lastSlide'
+          }
           // later move the person section into a separate component
           return (
-            <article key={id}>
-              <img src={image} alt={name} className='person-img'/>
+            <article className= {position} key={id}>
+              <img src={image} alt={name} className='person-img' />
               <h4>{name}</h4>
               <p className="title">{title}</p>
               <p className="text">{quote}</p>
-              <FaQuoteRight className='icon'/>
+              <FaQuoteRight className='icon' />
             </article>
           );
         })}
-      {/* later move the buttons into a separate component */}
+        {/* later move the buttons into a separate component */}
         <button className="prev">
           <FiChevronLeft />
         </button>
